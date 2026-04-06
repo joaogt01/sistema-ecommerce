0761f1bc7ec4150e1de0d196e5ea22200e9f7535
Diego Lyra <diegofernandes.lyra@gmail.com>
2026-04-06 15:12:51 -0300

    feat: classe pedido adicionada com: Pedido, adicionarItem, calcularTotal, finalizarPedido e gerarRecibo)

--- src/pedido.java
+++ src/pedido.java
@@ -11,8 +11,42 @@ public class Pedido {
         this.itens = new ArrayList<>();
         this.fechado = false;
         //this.dataCriacao = LocalDateTime.now();  
-}
+    }
 
     public void adicionarItem(ItemPedido item){
-        
+        if (fechado) {
+            System.out.println("Pedido já está fechado!");
+            return;
+        }
+        itens.add(item);
     }
+
+    public double calcularTotal(){
+        double total = 0;
+        for (ItemPedido item : itens){
+            total += item.getSubtotal();
+        }
+        return total;
+    }
+
+    public void finalizarPedido(){
+        this.fechado = true;
+    }
+
+    public void gerarRecibo(){
+        if (itens.isEmpty()){
+            System.out.println("Seu carrinho está vazio!");
+            return;
+        }
+
+        System.out.println("===== RECIBO =====");
+        System.out.println("Cliente: " + client.getNome());
+        for (ItemPedido item : itens){
+            Systrem.out.println(Item.GetProduto().getNome() + " | Quantidade: " + item.getQuantidade() + " | Subtotal : R$ " + item.getSubtotal());
+        }
+
+        System.out.println("------------------");
+        System.out.println("TOTAL: R$ " + CALCULARtOTAL());
+        Sytem.out.println("Status: " + (fechado ? "Fechado" : "Aberto"));
+    }
+}
